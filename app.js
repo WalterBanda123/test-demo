5925f65e1f16a2799408b397cab2d5d7a601d867
Walter Banda <walterbanda98@gmail.com>
2024-06-26 12:30:13 +0200

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express');
 
 const app = express();
-
+var someVar = 'Testing if this works'
+console.log(someVar);
 const server = app.listen(3000, () => {
     console.log('App running on port 3000');
 })
