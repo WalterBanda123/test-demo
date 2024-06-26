0df1bd9924801dec61df755a44f587360e365c1f
Walter Banda <walterbanda98@gmail.com>
2024-06-26 12:54:06 +0200

    Lts

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express');
 
 const app = express();
-
 const server = app.listen(3000, () => {
     console.log('App running on port 3000');
 })
+
 
 module.exports = server
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,11 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { "ignoreReadBeforeAssign": true }]
+    }
+  }
 ];
